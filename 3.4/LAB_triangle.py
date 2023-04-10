89105ac08bfa67efc917f2dbf00f6c1299e926d1
Charise Walraven <charise.walraven@darlingii.com>
2023-04-10 21:23:14 +0200

    formats file

--- 3.4/LAB_triangle.py
+++ 3.4/LAB_triangle.py
@@ -10,7 +10,7 @@
 # described by the three points; the perimeter is a sum of all legs' lengths (we mention it for the record, although we
 # are sure that you know it perfectly yourself.)
 
-import math # noqa
+import math  # noqa
 from LAB_points_on_a_plane import Point
 
 
