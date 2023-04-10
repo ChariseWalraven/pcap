e8e22acaa957798f36103a59fd3ca112354088b4
Charise Walraven <charise.walraven@darlingii.com>
2023-04-10 18:32:17 +0200

    fixes missing formatting; removes irrelevant instruction at end

--- 3.4/LAB_days_of_the_week.py
+++ 3.4/LAB_days_of_the_week.py
@@ -11,14 +11,12 @@
 # yourself; don't worry, we'll talk about the objective nature of exceptions soon). The class should provide the
 # following facilities:
 
-# objects of the class should be "printable", i.e. they should be able to implicitly convert themselves into strings of
-# the same form as the constructor arguments;
-# the class should be equipped with one-parameter methods called add_days(n) and subtract_days(n), with n being an
+# - objects of the class should be "printable", i.e. they should be able to implicitly convert themselves into strings
+# of the same form as the constructor arguments;
+# - the class should be equipped with one-parameter methods called add_days(n) and subtract_days(n), with n being an
 # integer number and updating the day of week stored inside the object in the way reflecting the change of date by the
 # indicated number of days, forward or backward.
-# all object's properties should be private;
-# Complete the template we've provided in the editor and run your code and check whether your output looks the same as
-# ours.
+# - all object's properties should be private;
 
 class WeekDayError(Exception):
     pass
